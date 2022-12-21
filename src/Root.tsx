67c8004115828319aa971b35ef48cfd953eb1ca3
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-21 18:42:07 +0530

    init app

---
+++ src/Root.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Root extends Component {
+	constructor(props: any) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return <div>Home</div>;
+	}
+}
+
+export default Root;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Home extends Component {
+     constructor(props: any) {
+          super(props);
+          this.state = {  };
+     }
+	render() {
+		return <div>Home</div>;
+	}
+}
