b13bc103da89d16961f1c0518d690fb53827dc5f
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-25 12:48:59 +0530

    add catalog page

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,7 +2,9 @@ import { Component } from "react";
 import "./App.css";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route } from "react-router";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
 import Profile from "./pages/Profile";
 import Error from "./pages/Error";
 import UserForm from "./pages/UserForm";
@@ -17,14 +19,19 @@ class Root extends Component {
 	render() {
 		return (
 			<HashRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/user" element={<UserForm />} />
-					<Route path="/user/:id" element={<Profile />} />
-					<Route path="/tweet" element={<TweetForm />} />
-					<Route path="/tweet/:id" element={<Thread />} />
-					<Route path="*" element={<Error />} />
-				</Routes>
+				<Header />
+				<div className="mt-5">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/explore" element={<Catalog />} />
+						<Route path="/user" element={<UserForm />} />
+						<Route path="/user/:id" element={<Profile />} />
+						<Route path="/tweet" element={<TweetForm />} />
+						<Route path="/tweet/:id" element={<Thread />} />
+						<Route path="*" element={<Error />} />
+					</Routes>
+				</div>
+
 			</HashRouter>
 		);
 	}
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -8,15 +8,18 @@ function Home(props: any): JSX.Element {
 		fetch("http://localhost:3000/users")
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
 				setusers(data);
 				setLoading(false);
 			});
 	}, []);
 	if (loading) return <div>Loading...</div>;
 	return (
-		<ul>
-			{users.map((user: any) => (User(user)))}
+		<ul className="list-group bg-dark text-bg-dark p-5 d-flex flex-column">
+			{users.map((user: any) => (
+				<li key={`${user._id}`} className="m-3 p-3 list-group-item-dark rounded-2">
+					{User(user)}
+				</li>
+			))}
 		</ul>
 	);
 }
