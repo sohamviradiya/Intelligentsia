140775aec45e3f0c3aa37e0a0a53d096c5016c8a
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-25 12:49:22 +0530

    set up auth socket

---
+++ src/modules/auth.ts
@@ -0,0 +1,67 @@
+import { baseurl } from "./config";
+
+export type AuthInterface = {
+	token: string | null;
+	username: string | null;
+	_id: string | null;
+};
+
+const AuthModule = (() => {
+	const LOGIN = async (
+		username: string,
+		password: string
+	): Promise<AuthInterface> => {
+		const response = await fetch(`${baseurl}/auth/`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ username, password }),
+		});
+		const data = await response.json();
+		if (!data.access_token) throw new Error("Unable to login");
+		localStorage.setItem("token", data.access_token);
+		return {
+			token: data.token,
+			username: username,
+			_id: null,
+		};
+	};
+
+	const PROFILE = async (): Promise<AuthInterface> => {
+		const token = localStorage.getItem("token");
+		if (!token) throw new Error("No token found");
+		const response = await fetch(`${baseurl}/auth/`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		const data = await response.json();
+		if (!data._id) throw new Error("Unable to get profile");
+		return { token: token, username: data.username, _id: data._id };
+	};
+
+	const LOGOUT = async (): Promise<AuthInterface> => {
+		const token = localStorage.getItem("token");
+		const response = await fetch(`${baseurl}/auth/`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		const data = await response.json();
+		if (!response.ok) throw new Error("Unable to logout");
+
+		localStorage.removeItem("token");
+		return { token: null, username: null, _id: null };
+	};
+
+	return {
+		LOGIN,
+		LOGOUT,
+		PROFILE,
+	};
+})();
+
+export default AuthModule;
