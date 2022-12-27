134da04ceee3c6573683436b18e4f52c52c029c2
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-27 23:14:58 +0530

    realign avatar on tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
 import { TweetInterface } from "../modules/tweet";
 import { baseurl } from "../modules/config";
+import { useEffect } from "react";
 function Tweet(tweet: TweetInterface): JSX.Element {
 	return (
 		<div className="card bg-dark border-primary">
 			<div className="row g-2">
-				<div className="col-4 px-3 py-3">
-					<img src={`${baseurl}/${tweet.user.avatar}`} className="border border-info" height={220} />
+				<div className="col-4 d-flex flex-row align-items-center justify-content-center">
+					<img src={`${baseurl}/${tweet.user.avatar}`} className="border border-info" height={250} />
 				</div>
 				<div className="col-8">
-					<div className="card-header display-6 d-flex flex-row justify-content-start">
-						<Link className="text-decoration-none" to={`/user/${tweet.user._id}`}>{tweet.user.username}</Link>
+
+					<div className="card-header h2 d-flex flex-row justify-content-start">
+						<Link className="card-title display-6 text-decoration-none text-primary" to={`tweet/${tweet._id}`}> {tweet.title} </Link>
 					</div>
 					<div className="card-body text-primary">
-						<h5 className="card-title"> {tweet.title} </h5>
-						<p className="card-text"> {tweet.content} </p>
+						<Link className="text-decoration-none h2" to={`/user/${tweet.user._id}`}>{tweet.user.username}</Link>
+						<p className="card-text h5 mt-3"> {tweet.content} </p>
+						<p className="card-text">Likes: {tweet.likes.length} </p>
 					</div>
 					<div className="card-footer text-primary">
 						{new Date(tweet.postedAt).toLocaleString()}
