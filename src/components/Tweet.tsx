e49977809210d49c60dce886031b0f13194aa564
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-27 16:47:38 +0530

    set tweetlists ui

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,7 +1,28 @@
-import { useParams } from "react-router-dom";
-function Tweet(props: any): JSX.Element {
-	const { id } = useParams();
-	return <div>Tweet {id}</div>;
+import { Link } from "react-router-dom";
+import { TweetInterface } from "../modules/tweet";
+import { baseurl } from "../modules/config";
+function Tweet(tweet: TweetInterface): JSX.Element {
+	return (
+		<div className="card bg-dark border-primary">
+			<div className="row g-2">
+				<div className="col-4 px-3 py-3">
+					<img src={`${baseurl}/${tweet.user.avatar}`} className="border border-info" height={220} />
+				</div>
+				<div className="col-8">
+					<div className="card-header display-6 d-flex flex-row justify-content-start">
+						<Link className="text-decoration-none" to={`/user/${tweet.user._id}`}>{tweet.user.username}</Link>
+					</div>
+					<div className="card-body text-primary">
+						<h5 className="card-title"> {tweet.title} </h5>
+						<p className="card-text"> {tweet.content} </p>
+					</div>
+					<div className="card-footer text-primary">
+						{new Date(tweet.postedAt).toLocaleString()}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Tweet;
---
+++ src/pages/Tweetlist.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useHref, useParams } from "react-router-dom";
+import { baseurl, tweetlist } from "../modules/config";
+import { TweetInterface } from "../modules/tweet";
+import Tweet from "../components/Tweet";
+function TweetList(props: any): JSX.Element {
+     const { id } = useParams();
+     const [tweets, setTweets] = useState<TweetInterface[]>([] as TweetInterface[]);
+     const [loading, setLoading] = useState(true);
+     useEffect(() => {
+          if (props.type == tweetlist.home) {
+               fetch(`${baseurl}/tweets/`).then((res) => (res.json())).then((data: TweetInterface[]) => {
+                    setTweets(data);
+                    setLoading(false);
+               });
+          } else if (props.type == tweetlist.authored) {
+               fetch(`${baseurl}/users/${id}/tweets/`).then((res) => (res.json())).then((data: TweetInterface[]) => {
+                    setTweets(data);
+                    setLoading(false);
+               });
+          } else if (props.type == tweetlist.liked) {
+               fetch(`${baseurl}/users/${id}/liked/`).then((res) => (res.json())).then((data: TweetInterface[]) => {
+                    setTweets(data);
+                    setLoading(false);
+               });
+          } else {
+               setTweets([]);
+               setLoading(false);
+          }
+     }, [id, props.type]);
+
+     return (
+          (loading) ?
+               (<div className="position-absolute top-50 start-50 translate-middle">Loading...</div>)
+               : (
+                    <ul className="container w-75 p-5">
+                    {tweets.map((tweet: any) => (
+                         <li
+                              key={tweet._id}
+                              className="mb-5">
+                              {Tweet(tweet)}
+                         </li>
+                    ))}
+               </ul>)
+     );
+}
+export default TweetList;
