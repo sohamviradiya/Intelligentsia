15d48582b19f43a6f23643c11d801c11461735d9
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-29 21:49:29 +0530

    add user deletion

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet, useParams } from "react-router-dom";
+import { NavLink, Outlet, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { UserInterface } from "../modules/user";
 import Loading from "../components/Loading";
@@ -10,6 +10,7 @@ function Profile(props: any): JSX.Element {
 	const [user, setUser] = useState<UserInterface>({} as UserInterface);
 	const [loading, setLoading] = useState(true);
 	const [self, setSelf] = useState(false);
+	const navigate = useNavigate();
 	useEffect(() => {
 		if (!id) throw new Error("No id provided");
 		fetch(`${baseurl}/users/${id}`).then(res => res.json()).then(data => {
@@ -20,6 +21,23 @@ function Profile(props: any): JSX.Element {
 			if (data._id === id) setSelf(true);
 		});
 	}, []);
+
+	const deleteUser = async () => {
+		const res = await fetch(`${baseurl}/users/`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${localStorage.getItem("token")}`
+			}
+		});
+		const data = await res.json();
+		if (res.status === 200) {
+			localStorage.removeItem("token");
+			navigate("/");
+		} else {
+			console.log(data);
+		}
+	};
 
 	return (
 		loading ? <Loading /> :
@@ -43,6 +61,7 @@ function Profile(props: any): JSX.Element {
 					<section className="d-flex flex-row p-3 gap-3">
 						<NavLink className="btn btn-primary" to="/user">Edit Profile</NavLink>
 						<NavLink className="btn btn-primary" to="/tweet">Post Tweet</NavLink>
+						<button className="btn btn-danger" onClick={deleteUser}>Delete Account</button>
 					</section>) : (<></>)
 				}
 				<section className="mt-5 min-vh-100">
